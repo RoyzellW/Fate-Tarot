38e68965f5260d707126183379bc4e2588dc4599
student <royzellwalton@gmail.com>
2018-07-23 09:32:44 -0500

    In the process of adding a fucntion that will check for the win condition. Also in the process of adding code that will limit the hand count for both parties to 5. Also trying to solve the main issue of being able to correctly discern which cards will be printed out and which value will be given. I Would also liketo be able to give the cards name plus the value within the game and not just the value.

--- FateTarotCardGame.py
+++ FateTarotCardGame.py
@@ -1,9 +1,10 @@
+import os
 import random
-from random import shuffle
 
 
 
 The_Fool = 0
+The_Fool_Reversed = 0
 The_Magician = 1
 The_Magician_Reversed = -1
 The_High_Priestess = 2
@@ -50,7 +51,7 @@ The_World_Reversed = -20
 class DeckList():
 
     DeckValues = [
-        [0],
+        [0, 0],
         [1, -1],
         [2, -2],
         [3, -3],
@@ -72,7 +73,7 @@ class DeckList():
         [5, -19],
         [5, -20]]
 
-    DeckValues[0] = [The_Fool]
+    DeckValues[0] = [The_Fool, The_Fool_Reversed]
     DeckValues[1] = [The_Magician, The_Magician_Reversed]
     DeckValues[2] = [The_High_Priestess, The_High_Priestess_Reversed]
     DeckValues[3] = [The_Empress, The_Empress_Reversed]
@@ -108,7 +109,7 @@ def deal():
         hand.append(card)
     return hand
 
-def draw(hand):
+def draw(dealerHand, playerHand):
     card = DeckList.DeckValues.pop()
     hand.append(card)
     return hand
@@ -130,4 +131,78 @@ def playAgain():
         print("Bye!")
         exit()
 
+def clear():
+    if os.name == 'nt':
+        os.system('CLS')
+    if os.name == 'posix':
+        os.system('clear')
+
+def printResults(dealerHand, playerHand):
+    clear()
+    print("The dealer has a " + str(dealerHand) + "for a total of" + str(total(dealerHand)))
+    print("You have a " + str(playerHand) + " for a total of " + str(total(playerHand)))
+
+def fateTarot(dealerHand, playerHand):
+    if total(playerHand) > dealerHand:
+        printResults(dealerHand, playerHand)
+        print("Congratualitions! You fought Fate and won!")
+        playAgain()
+    if total(playerHand) < dealerHand:
+        printResults(dealerHand, playerHand)
+        print("Sorry! You fought Fate and lost!")
+        playAgain()
+
+def results(dealerHand, playerHand):
+    if(dealerHand == DeckList.DeckValues[0][0]):
+        print("You win!!!!")
+    elif(dealerHand == DeckList.DeckValues[0]):
+        print(DeckList.DeckValues[:0])
+        print("Dealer has lost! You have been spared by Fate!")
+
+    elif(dealerHand == DeckList.DeckValues[0]):
+        print(DeckList.DeckValues[0])
+        print("Dealer has won! You fought Fate and lost!")
+
+    elif(playerHand == DeckList.DeckValues[:0]):
+        print(DeckList.DeckValues[:0])
+        print("You have lost! The dealer has been favored by Fate!")
+
+    elif(playerHand == DeckList.DeckValues[0]):
+        print(DeckList.DeckValues[0])
+        print("You have won! You have been favored by Fate!")
+
+
+def game():
+    choice = 0
+    clear()
+    dealerHandCount = 1
+    playerHandCount = 1
+    maxPlays = 5
+    print ("WELCOME TO Fate Tarot\n")
+    dealerHand = deal(DeckList.DeckValues)
+    playerHand = deal(DeckList.DeckValues)
+    while choice != "q":
+        print ("The dealer has drawn " + str(dealerHand[0]))
+        print ("You have drawn " + str(playerHand) + " for a total of " + str(total(playerHand)))
+        fateTarot(dealerHand, playerHand)
+        choice = input("Do you want to [H]it, [S]tand, or [Q]uit: ").lower()
+        clear()
+        if choice == "h":
+            draw(playerHand)
+            while total(dealerHand) < total():
+                playerHandCount+=1
+                if playerHandCount > maxPlays:
+                    print("out of plays")
+                    break
+                draw(dealerHand)
+            results(dealerHand, playerHand)
+            playAgain()
+        elif choice == "s":
+            while total(dealerHand) < :
+                draw(dealerHand)
+            draw(dealerHand, playerHand)
+            playAgain()
+        elif choice == "q":
+            print("Bye!")
+            exit()
 
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Fate-Tarot
+
+Basically the objective of the game, is to have more points than your opponent
+at the end of the game. 
+Points are calculated based on the card you pick and the position it is in. 
+Each card has a numerical value from 0 - 20. 
+The numerical value of each card only changes in terms of it being inverse.
+For example; the world card has a numerical value of 20 while upright. 
+If the player draws the card upright then they receive 20 points. 
+But if the card is drawn upside down then the inverse happens.
+They instead must subtract 20 points. 
+This rule applies to every card except for the card with a value of 0.
+This card creates an exception, and can either make you win or lose the game.
+The fool is the card with a numerical value of 0 regardless of its position. 
+If the fool is drawn upright then you automatically win. 
+If the fool is drawn upside down then you automatically lose.
+The game ends if the fool is drawn, but if it is not drawn;
+the game continues on until a maximum of 5 cards for each side has been drawn.
+After 5 cards on both sides have been drawn the results are calculated-
+and shown. 
+A winner will be picked based on whom had the higher score. 
+It is possible for a player to have a score within the negatives.
+
+
